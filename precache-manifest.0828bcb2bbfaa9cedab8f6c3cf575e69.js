12fa20138352d457f073189303df91be1102e93b
HarshDubey <harshchandradubey@gmail.com>
2020-08-23 20:57:34 +0530

    Updates

---
+++ precache-manifest.0828bcb2bbfaa9cedab8f6c3cf575e69.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ec39f4d2ff5c46e9c958cf098d90f4",
+    "url": "/reactlive/index.html"
+  },
+  {
+    "revision": "d964e47a535ece84ddd9",
+    "url": "/reactlive/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8c1cae395abd3a0377da",
+    "url": "/reactlive/static/css/main.9cbadfba.chunk.css"
+  },
+  {
+    "revision": "d964e47a535ece84ddd9",
+    "url": "/reactlive/static/js/2.2e9d362f.chunk.js"
+  },
+  {
+    "revision": "c18ad82b3be9985ffcbc3de3c04c0198",
+    "url": "/reactlive/static/js/2.2e9d362f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1cae395abd3a0377da",
+    "url": "/reactlive/static/js/main.364768ad.chunk.js"
+  },
+  {
+    "revision": "0d62083387a57a6396a5",
+    "url": "/reactlive/static/js/runtime-main.7706b920.js"
+  },
+  {
+    "revision": "2522fc5b0547e2d158fd281005b950dd",
+    "url": "/reactlive/static/media/add.2522fc5b.ico"
+  },
+  {
+    "revision": "ce4dde0a3bd9c9b7b408a7b80d918d2f",
+    "url": "/reactlive/static/media/back.ce4dde0a.ico"
+  },
+  {
+    "revision": "e9ff9e0a9b99506b31a4724aeb132509",
+    "url": "/reactlive/static/media/delete.e9ff9e0a.ico"
+  },
+  {
+    "revision": "f9cb8892659effdef060c9a9dd5c8130",
+    "url": "/reactlive/static/media/edit.f9cb8892.ico"
+  },
+  {
+    "revision": "74ce074911effd4f5f1f5018e38efc44",
+    "url": "/reactlive/static/media/email.74ce0749.ico"
+  },
+  {
+    "revision": "c10a4e3e871df73e561d2e40307e5be5",
+    "url": "/reactlive/static/media/name.c10a4e3e.ico"
+  },
+  {
+    "revision": "5be69fc93e805d3261edd1264a2c867b",
+    "url": "/reactlive/static/media/pass.5be69fc9.ico"
+  },
+  {
+    "revision": "1c855f7097a86a7628dc801522a48fd9",
+    "url": "/reactlive/static/media/print.1c855f70.ico"
+  },
+  {
+    "revision": "8a3c9d68f77c0dd78fd321ac2cc22b2f",
+    "url": "/reactlive/static/media/repass.8a3c9d68.ico"
+  },
+  {
+    "revision": "78180536e74c86043518a6915a442ed4",
+    "url": "/reactlive/static/media/view.78180536.ico"
+  }
+]);
